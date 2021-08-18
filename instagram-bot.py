517ea974e34ec79dfe95fd83ad41cb12f726082d
meltingroot <jangyk00@outlook.com>
2021-08-18 22:10:40 +0900

    20210818

--- instagram-bot.py
+++ instagram-bot.py
@@ -3,8 +3,8 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import python_config
-login_id = python_config.getXml('INSTAGRAM_LOGIN_INFO', 'LOGIN_ID')
-login_password = python_config.getXml('INSTAGRAM_LOGIN_INFO', 'LOGIN_PASSWORD')
+login_id = python_config.getXml('INSTAGRAM_LOGIN_INFO', 'LOGIN_ID') #instagram id
+login_password = python_config.getXml('INSTAGRAM_LOGIN_INFO', 'LOGIN_PASSWORD') #instagram password
 ## login info
 
 
@@ -13,21 +13,55 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pyautogui
 
-driver = webdriver.Edge("driver/msedgedriver.exe")
+#browser driver
+driver = webdriver.Edge("instagram-bot/driver/msedgedriver.exe") 
 
 driver.get('https://www.instagram.com/accounts/login/')
-sleep(10)
+sleep(5)
 driver.find_element_by_css_selector('[name=username]').send_keys(login_id)
 sleep(3)
 driver.find_element_by_css_selector('[name=password]').send_keys(login_password)
 sleep(3)
 driver.find_element_by_css_selector('[name=password]').send_keys(Keys.ENTER)
-sleep(10)
+sleep(3)
 driver.get('https://www.instagram.com/explore/tags/%EA%B0%95%EC%95%84%EC%A7%80/')
 sleep(10)
-pyautogui.press('tab',presses=3)
-imageUrl = driver.find_element_by_css_selector('[class=focus-visible]').get_attribute("href")
-print(imageUrl)
+
+elements = driver.find_elements_by_tag_name('a')
+
+count = 1
+maxCount = 300
+elementCount = 0
+while (count <= maxCount) :
+    element = elements[elementCount]
+
+    imageUrl = element.get_attribute("href")
+    print(str(count) + ' : ' + imageUrl)
+
+    if (imageUrl.find("/p/") < 0) :
+        driver.execute_script("""
+            window.scrollTo(0,10000);
+        """)
+        sleep(10)
+
+    else : 
+        driver.execute_script("""
+            document.getElementsByTagName("a")[0].remove();
+        """)
+
+        if (elementCount%10 == 0) : 
+            driver.execute_script("""
+                window.scrollTo(0,5000);
+            """)
+            sleep(3)
+            elements = driver.find_elements_by_tag_name('a')
+            elementCount = 0
+        
+        elementCount = elementCount + 1
+
+    count = count + 1
+
+print("process finish")
 
 
 
